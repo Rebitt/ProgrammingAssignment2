a995c1502867628ba6bec4fc1789bf9cf5024558
Rebitt <rebecahuerga@gmail.com>
2016-07-16 10:08:37 +0200

    First commit test
    Trying to understand github.

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+## Rebeca Huerga assignments
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
